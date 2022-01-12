ff02d7165315cedcf1294f9af0258c5ceed24a0b
Ki-Seki <Ki_Seki@outlook.com>
2022-01-12 11:06:04 +0800

    use weighted average approach

--- fitness.m
+++ fitness.m
@@ -1,11 +1,28 @@
-% 多目标适应度函数，计算粒子（群） p 的配送总时间和总成本
-function [T, Z] = fitness(p, field)
-    % p 粒子、多个例子构成的粒子群（此时一行代表一个粒子）
+% 多目标适应度函数，计算粒子群 p 的配送总时间和总成本
+function fit = fitness(p, field, matrix)
+    % p 粒子群，每行是一个粒子（允许只有一个粒子）
     % field 数据集
-    % T 总时间；如果 p 是粒子群，那么 T 是一个列向量
-    % Z 总成本；如果 p 是粒子群，那么 Z 是一个列向量
+    % matrix 邻接矩阵
+    % fit 适应度矩阵，每行表示一个粒子的适应度，包含 T 总时间和 Z 总成本两个元素
 
+    [m, n] = size(p);
+    for i = 1 : m  % 遍历每一个粒子
+        t = 0;  % 总时间
+        z = 0;  % 总成本
+        now_p = [0, p(i, :)];  % 补充配送原点到当前粒子，方便后续循环
+        load = 0;  % 载重
+        vehicle = 0;  % 车辆使用数量
+        for j = 2 : n+1  % 遍历每一个需求点
+            demand = field.DEMAND(now_p(j), 2);
+            if load + demand <= field.VEHICLE_CAPACITY
+                load = load + demand;
+            end
+        end
+    end
+    
+    
     % 以下是占位数据用于临时测试
-    T = ones(size(p));
-    Z = ones(size(p));
+    T = rand(size(p, 1), 1);
+    Z = rand(size(p, 1), 1);
+    fit = [T, Z];
 end
--- main.m
+++ main.m
@@ -1,19 +1,21 @@
 % 用优化粒子群算法解决带有风险矩阵的 TSP 问题
 
-% 一些假定或简化
-% 初代版本首先将多辆车问题转化为单辆车问题，在结果上用×或÷车数来表示以简化问题
-% 速度表示方法先用最简单的随即方法来做，回头再做优化
-% 弗洛伊德算法可继续优化
-
 % 算法特色
 % 风险矩阵、偏移速度表示方法、多目标适应度函数
 
 % 注意事项
 % 结点从 0 开始编号，但是 MATLAB 是从 1 开始编号的
+% 函数名后加一下划线表示重载的函数
+% VRP 问题背景下，适应度值即为路径长度、成本等，适应度越小越好
+% 适应度均用矩阵表示，以增强程序通用性
+
+% 待完善 or 待优化
+% 弗洛伊德算法：可继续优化
+% 速度表示：暂时采用随机方法
+% 适应度排序：暂时采用加权方法
 
 % TODO
-% 适应度函数是加权综合一个值好，还是双目标，如果是双目标，怎么双得好
-% 适应度函数还没编写完成
+% fitness 函数
 
 clear;
 clc;
@@ -30,6 +32,9 @@ w = 0.4;  % 惯性权重
 c1 = 2;  % 学习因子，下同
 c2 = 2;
 
+coeff_t = 0.95;  % 目标 T 的权重
+coeff_z = 0.05;  % 目标 Z 的权重
+
 %% 初始化
 
 field = read_dataset(dataset);  % 读数据集到 field 结构体，它包含数据集中所有字段值
@@ -41,9 +46,12 @@ for i = 1 : particle_cnt
     particle(i, :) = randperm(field.NODE_COUNT);
 end
 
-[pT, pZ] = fitness(particle, field);  % 计算适应度
-pBest = particle;
-% TODO
+fit = fitness(particle, field);  % 适应度是一个两列（T 和 Z）的矩阵
+[~, index] = min_(fit, coeff_t, coeff_z);  % 使用重载函数找最优值
+p_best = particle;  % 个体最优对应的粒子群
+g_best = particle(index, :);  % 全局最优对应的粒子
+p_best_fit = fit;  % 个体最优值
+g_best_fit = fit(index, :);  % 全局最优值
 
 %% 程序核心循环
 for i = 1 : loop_cnt
