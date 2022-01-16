643edd43402d3b0ed96190d03b4a6fdb2aa05ad6
Ki-Seki <Ki_Seki@outlook.com>
2022-01-16 11:02:59 +0800

    增强fitness函数通用性

--- fitness.m
+++ fitness.m
@@ -1,9 +1,12 @@
 % 多目标适应度函数，计算粒子群 p 的配送总时间和总成本
-function fit = fitness(particle, field, matrix)
+function [fit, vehicle, d, r] = fitness(particle, field, matrix)
     % particle 粒子群，每行是一个粒子（允许只有一个粒子）
     % field 数据集
     % matrix 邻接矩阵
     % fit 适应度矩阵，每行表示一个粒子的适应度，包含 T 总时间和 Z 总成本两个元素
+    % vehicle 车辆使用情况数组，每个元素代表一辆车，元素值是该车服务需求点数
+    % d 总运距
+    % r 总消杀次数
 
     [m, n] = size(particle);  % 粒子个数 需求点个数
     fit = zeros(m, 2);  % 返回值
@@ -15,7 +18,7 @@ function fit = fitness(particle, field, matrix)
         %% 求车辆使用情况
         
         k = 1;  % 车辆使用数量
-        vehicle = [0];  % 车辆使用情况数组，每个元素代表一辆车，元素值是该车服务需求点数
+        vehicle = [0];  % 车辆使用情况数组
         load = 0;  % 当前载重
         for j = 1 : n  % 遍历每一个需求点，其编号为 p(j)
             demand = field.DEMAND(p(j), 2);  % 需求点需求量
