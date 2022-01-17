8fc118e107c6f0f2f4774b137f6e9956767ecd33
Ki-Seki <Ki_Seki@outlook.com>
2022-01-17 11:25:38 +0800

    fitness.m 测试完毕

--- fitness.m
+++ fitness.m
@@ -1,15 +1,21 @@
 % 多目标适应度函数，计算粒子群 p 的配送总时间和总成本
-function [fit, vehicle, d, r] = fitness(particle, field, matrix)
+function [fit, vehicle, dist, risk] = fitness(particle, field, matrix)
     % particle 粒子群，每行是一个粒子（允许只有一个粒子）
     % field 数据集
     % matrix 邻接矩阵
     % fit 适应度矩阵，每行表示一个粒子的适应度，包含 T 总时间和 Z 总成本两个元素
-    % vehicle 车辆使用情况数组，每个元素代表一辆车，元素值是该车服务需求点数
-    % d 总运距
-    % r 总消杀次数
+    % 以下返回值按需取用
+    % vehicle 元胞数组，每行是一个车辆使用情况数组，其中每个元素代表一辆车，元素值是该车服务需求点数
+    % dist 总运距数组，每行代表一个粒子的总运距
+    % risk 总消杀次数数组，每行代表一个粒子的总消杀次数
 
-    [m, n] = size(particle);  % 粒子个数 需求点个数
-    fit = zeros(m, 2);  % 返回值
+    [m, n] = size(particle);  % 获取粒子个数和需求点个数
+    
+    % 返回值初始化
+    fit = zeros(m, 2);
+    vehicle = cell(m, 1);
+    dist = zeros(m, 1);
+    risk = zeros(m, 1);
     
     for i = 1 : m  % 遍历每一个粒子
         
@@ -18,16 +24,16 @@ function [fit, vehicle, d, r] = fitness(particle, field, matrix)
         %% 求车辆使用情况
         
         k = 1;  % 车辆使用数量
-        vehicle = [0];  % 车辆使用情况数组
+        v = [0];  % 车辆使用情况数组
         load = 0;  % 当前载重
         for j = 1 : n  % 遍历每一个需求点，其编号为 p(j)
             demand = field.DEMAND(p(j), 2);  % 需求点需求量
             if load + demand > field.VEHICLE_CAPACITY  % 若车载不足
                 k = k + 1;  % 车数加一
-                vehicle = [vehicle 0];  % 车数加一
+                v = [v 0];  % 车数加一
                 load = 0;  % 载重清空
             end
-            vehicle(k) = vehicle(k) + 1;  % 第 k 辆车服务数量加一
+            v(k) = v(k) + 1;  % 第 k 辆车服务数量加一
             load = load + demand;  % 增加载重
         end
         
@@ -37,7 +43,7 @@ function [fit, vehicle, d, r] = fitness(particle, field, matrix)
         r = 0;  % 总消杀次数
         st = 1;  % 某辆车起始服务点，p(st) 是服务点编号
         for j = 1 : k  % 遍历每一辆车
-            ed = st + vehicle(j) - 1;  % 某辆车终止服务点，p(ed) 是服务点编号
+            ed = st + v(j) - 1;  % 某辆车终止服务点，p(ed) 是服务点编号
             
             % 注意：下面常出现 p()，一般要令其 +1，是因为 matrix 是包括原点的
             
@@ -60,8 +66,11 @@ function [fit, vehicle, d, r] = fitness(particle, field, matrix)
             k * field.VEHICLE_FIXED_COST + ...
             r * field.VEHICLE_DISINFECTION_COST;  % 元
         
-        %% 将当前粒子的总时间与总成本代入适应度矩阵
+        %% 更新返回值
         
         fit(i, :) = [t, z];
+        vehicle{i} = v;
+        dist(i) = d;
+        risk(i) = r;
     end
 end
