2ce5b3a181ea015609f88dc145b14dbdbb94cf60
Ki-Seki <Ki_Seki@outlook.com>
2022-01-19 18:08:44 +0800

    修改适应 example 的参数；核心循环编写；其他优化

--- main.m
+++ main.m
@@ -1,5 +1,8 @@
 % 用优化粒子群算法解决带有风险矩阵的 TSP 问题
 
+% 运行环境
+% 
+
 % 算法特色
 % 风险矩阵、偏移速度表示方法、多目标适应度函数
 
@@ -9,6 +12,8 @@
 % VRP 问题背景下，适应度值即为路径长度、成本等，适应度越小越好
 % 适应度均用矩阵表示，以增强程序通用性
 % 邻接矩阵的下标从配送原点开始算起
+% min_.m 函数暂时未使用
+% is_le.m 函数暂时未使用
 
 % 程序约束
 % 数据集中车载重必须大于单一需求点需求量，否则程序出现 BUG
@@ -35,38 +40,91 @@ rand_seed = 1;  % 随机数种子
 
 dataset = 'example';  % 数据集名称
 
-loop_cnt = 100;  % 进化次数
-particle_cnt = 100;  % 个体数目
+loop_cnt = 60;  % 进化次数
+particle_cnt = 3;  % 个体数目
 
-w = 0.4;  % 惯性权重
-c1 = 2;  % 学习因子，下同
-c2 = 2;
+w = 1.5;  % 惯性权重
+c1 = 4;  % 自我学习因子
+c2 = 4;  % 群体学习因子
 
-coeff_t = 0.95;  % 目标 T 的权重
-coeff_z = 0.05;  % 目标 Z 的权重
+coeff.t = 0.95;  % 目标 T 的权重
+coeff.z = 0.05;  % 目标 Z 的权重
 
 %% 初始化
 
-rand(rand_type, rand_seed);
+rand(rand_type, rand_seed);  % 随机数生成器初始化
+convergence = 0;  % 收敛时的迭代次数
 field = read_dataset(dataset);  % 读数据集到 field 结构体，它包含数据集中所有字段值
+figure(1);
 draw_net(field);  % 绘制结点网络图
+
 matrix = floyd_algo(field.NODE, field.EDGE);  % 用弗洛伊德算法求邻接矩阵
 particle = zeros(particle_cnt, field.NODE_COUNT-1);  % 创建粒子种群
-
-% 为每个粒子生成随机的路径序列
 for i = 1 : particle_cnt
-    particle(i, :) = randperm(field.NODE_COUNT-1);
+    particle(i, :) = randperm(field.NODE_COUNT-1);  % 为每个粒子生成随机的路径序列
 end
+velocity = rands(particle_cnt, field.NODE_COUNT-1);  % 初始化粒子速度
 
 fit = fitness(particle, field, matrix);  % 适应度是一个两列（T 和 Z）的矩阵
-[~, index] = min_(fit, coeff_t, coeff_z);  % 使用重载函数找最优值
+[~, index] = min(weighted(fit, coeff));  % 找群体最优值对应下标
 p_best = particle;  % 个体最优对应的粒子群
 g_best = particle(index, :);  % 全局最优对应的粒子
 p_best_fit = fit;  % 个体最优值
 g_best_fit = fit(index, :);  % 全局最优值
 
-%% 程序核心循环
+%% 粒子群算法核心循环
 
+figure(2);
 for i = 1 : loop_cnt
-    % TODO
+    for j = 1 : particle_cnt
+        %% 计算速度与位置
+        
+        v = velocity(j, :);  % 当前粒子速度
+        x = particle(j, :);  % 当前粒子位置
+        
+        v = w * v + c1 * rand * (p_best(j, :)-x) + ...
+            c2 * rand * (g_best-x);  % 速度更新公式
+        x = x + v;  % 位置更新公式
+        
+        [velocity(j, :), particle(j, :)] = validate(v, x);  % 合法化速度与位置
+        
+        %% 更新个体最优
+        
+        tmp = fitness(particle(j, :), field, matrix);
+        if weighted(tmp, coeff) <= weighted(p_best_fit(j, :), coeff)
+            p_best(j, :) = particle(j, :);
+            p_best_fit(j, :) = tmp;
+        end
+    end
+    
+    %% 更新群体最优
+    [~, index] = min(weighted(p_best_fit, coeff));
+    tmp = p_best_fit(index, :);
+    if weighted(tmp, coeff) < weighted(g_best_fit, coeff)
+        g_best = p_best(index, :);
+        g_best_fit = tmp;
+        convergence = i;  % 更新收敛时的迭代次数
+    end
+    
+    %% 绘图
+    
+    disp(weighted(g_best_fit, coeff));
+    disp(g_best);
+    %disp(p_best(1, :));
+    plot(i, weighted(g_best_fit, coeff), '.r');
+    hold on;
 end
+
+%% 绘图
+
+% 结束收敛过程的绘图
+title(['PSO 收敛过程（数据集：', dataset, '）']);
+xlabel('迭代次数');
+ylabel('加权适应度值');
+hold off;
+
+% 绘制最优配送安排
+% TODO
+
+%% 输出最佳结果的相关数据
+disp(convergence);
