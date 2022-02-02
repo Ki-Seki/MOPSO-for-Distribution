82112348233cca4c7f04efbe431ce9031c7a680e
Ki-Seki <Ki_Seki@outlook.com>
2022-02-02 11:10:26 +0800

    完成修改 draw_convergence

--- draw_convergence.m
+++ draw_convergence.m
@@ -1,18 +1,35 @@
 % 绘制 PSO 收敛过程图
 function draw_convergence(f, conv, field)
-    % f 历次迭代的全局最优的适应度值
-    % conv 收敛时的迭代次数
+    % f 记录迭代：每行包括两目标每次在帕累托前沿中的平均值
+    % conv 两目标收敛时分别的迭代次数
+    % field 数据集
     
-    figure('Name','PSO 收敛过程图','NumberTitle','off')
+    figure('Name','MOPSO 收敛过程','NumberTitle','off')
+    suptitle(['MOPSO 两目标收敛过程图（数据集：', field.DATASET, '）']);
     
-    plot(f, '.b');
+    %% 绘制 T 的收敛过程
+    
+    subplot(2, 1, 1);
+    plot(f(:, 1), '- .r');
     hold on;
-    p = plot(conv, f(conv), '*r');
-    label = ['(' num2str(conv) ', ' num2str(f(conv)) ')'];  % 收敛点的标签
+    pt = plot(conv.t, f(conv.t, 1), 'or');
+    labelt = [' (' num2str(conv.t) ', ' num2str(f(conv.t, 1)) ')'];  % 收敛点的标签
     
-    title(['PSO 收敛过程图（数据集：', field.DATASET, '）']);
     xlabel('迭代次数');
-    ylabel('加权适应度值');
-    legend(p, ['收敛点' label]);
+    ylabel('目标 1：T（小时）');
+    legend(pt, ['收敛点' labelt]);
+    hold off;
+    
+    %% 绘制 Z 的收敛过程
+    
+    subplot(2, 1, 2);
+    plot(f(:, 2), '- .r');
+    hold on;
+    pz = plot(conv.z, f(conv.z, 2), 'or');
+    labelz = [' (' num2str(conv.z) ', ' num2str(f(conv.z, 2)) ')'];  % 收敛点的标签
+    
+    xlabel('迭代次数');
+    ylabel('目标 2：Z（元）');
+    legend(pz, ['收敛点' labelz]);
     hold off;
 end
